09fe5f61ac0851fa214e2847094d8221c967ebf4
DeltaRager <abhinavtj@gmail.com>
2021-11-12 22:12:19 +0530

    folder test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+const prefix = '$'
+
+//require('./modules/RegisterMember')
+
 client.on('ready', () => {
     console.log("Bot is up")
 })
