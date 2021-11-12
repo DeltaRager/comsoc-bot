a16346493a0b80d93d7a69ba0b4ea012731dacb7
DeltaRager <abhinavtj@gmail.com>
2021-11-12 22:50:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] });
 
 const prefix = '$'
 
@@ -8,10 +8,6 @@ const prefix = '$'
 client.once('ready', () => {
 	console.log('Bot is up');
 });
-
-client.on('mess', message => {
-    console.log(message.channel.type)
-})
 
 client.on('messageCreate', message => {
     console.log(message)
