bacf0a1c4dcf2d6d05a32443f539d744a7e5d900
DeltaRager <abhinavtj@gmail.com>
2021-11-12 23:04:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+
 const client = new Client({ 
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS],
     partials: ['MESSAGE', 'CHANNEL', 'REACTION']
