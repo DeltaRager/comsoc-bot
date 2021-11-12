a3b8ec3487647dfcab09b1e1418c6973dfd5cb20
DeltaRager <abhinavtj@gmail.com>
2021-11-12 22:00:43 +0530

    New try

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const discord = require('discord.js')
-const client = new discord.Client()
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
 client.on('ready', () => {
     console.log("Bot is up")
