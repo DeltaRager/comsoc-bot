c6f64b87894ff38e22cdb40ce3feedb03d6f4623
DeltaRager <abhinavtj@gmail.com>
2021-11-12 22:17:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,22 @@ const prefix = '$'
 
 //require('./modules/RegisterMember')
 
-client.on('ready', () => {
-    console.log("Bot is up")
-})
+client.once('ready', () => {
+	console.log('Bot is up');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
 
 client.login(process.env.TOKEN)
