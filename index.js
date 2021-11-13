4dabd7db3d5a582494821973bf39d36a715170b7
DeltaRager <abhinavtj@gmail.com>
2021-11-13 17:10:14 +0530

    Modularity part 2

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+
+
+const client = new Client({ 
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_PRESENCES],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER']
+});
+
+const prefix = '$'
+
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+//require('./modules/RegisterMember')
+
+for (const file of eventFiles) {
+	const event = require(`./src/events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, prefix, client));
+	}
+}
+
+client.login('NTE1MDA0OTAwMTUzNzUzNjIx.W_YhsA.RvcMANwI2t0Hm6NREq-ObbmKJCM')
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,6 +1,20 @@
+const fs = require('fs');
+const modules = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
 module.exports = {
     name: 'messageCreate',
-    execute(message) {
-        console.log(message.content);
+    execute(message, prefix, client) {
+        const contents = message.content.split(prefix)[1]
+        if(contents)
+        {
+            const command =  `${contents.split(' ')[0]}.js`
+            const args = contents.split(' ').splice(1,contents.length + 1)
+    
+            if(modules.find(cmd => cmd === command)){
+                const cmdcall = require(`../commands/${command}`)
+                cmdcall.execute(message, args, client)
+                
+            }
+        }
     }
 }
